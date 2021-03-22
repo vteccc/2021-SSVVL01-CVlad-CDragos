b9118635870dfbc83062f1d01e9563d01a3538d7
Corocea Vlad-Laurentiu <corocea.vlad@yahoo.com>
2021-03-22 22:42:33 +0200

    added service test

---
+++ src/test/java/ssvv/example/service/ServiceTest.java
@@ -0,0 +1,18 @@
+package ssvv.example.service;
+
+import org.junit.Test;
+import ssvv.example.repository.NotaXMLRepository;
+import ssvv.example.repository.StudentXMLRepository;
+import ssvv.example.repository.TemaXMLRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    private StudentXMLRepository studentXMLRepository;
+    private TemaXMLRepository  temaXMLRepository;
+    private NotaXMLRepository notaXMLRepository;
+
+
+
+}
