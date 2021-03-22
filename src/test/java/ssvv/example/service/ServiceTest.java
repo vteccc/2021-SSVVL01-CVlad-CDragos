4d4759f238f82f5f121895c76c49b18719d91589
Corocea Vlad-Laurentiu <corocea.vlad@yahoo.com>
2021-03-22 23:01:03 +0200

    changed saveStudent test

--- src/test/java/ssvv/example/service/ServiceTest.java
+++ src/test/java/ssvv/example/service/ServiceTest.java
@@ -1,18 +1,12 @@
 package ssvv.example.service;
 
-import org.junit.Test;
-import ssvv.example.repository.NotaXMLRepository;
-import ssvv.example.repository.StudentXMLRepository;
-import ssvv.example.repository.TemaXMLRepository;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServiceTest {
 
-    private StudentXMLRepository studentXMLRepository;
-    private TemaXMLRepository  temaXMLRepository;
-    private NotaXMLRepository notaXMLRepository;
-
-
-
+    @Test
+    void saveStudent() {
+    }
 }
