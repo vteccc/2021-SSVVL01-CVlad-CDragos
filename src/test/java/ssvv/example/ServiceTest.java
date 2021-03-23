dd02b2b4aa64b6d249828a5c792922211cfbcc2c
Corocea Vlad-Laurentiu <corocea.vlad@yahoo.com>
2021-03-23 02:53:20 +0200

    plz work2

--- src/test/java/ssvv/example/ServiceTest.java
+++ src/test/java/ssvv/example/ServiceTest.java
@@ -1,12 +1,27 @@
 package ssvv.example;
 
 import org.junit.jupiter.api.Test;
+import ssvv.example.repository.NotaXMLRepository;
+import ssvv.example.repository.StudentXMLRepository;
+import ssvv.example.repository.TemaXMLRepository;
+import ssvv.example.service.Service;
+import ssvv.example.validation.NotaValidator;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServiceTest {
 
+    private final Service service = new Service(
+            new StudentXMLRepository(new StudentValidator(), "studenti.xml"),
+            new TemaXMLRepository(new TemaValidator(), "teme.xml"),
+            new NotaXMLRepository(new NotaValidator(), "note.xml"));
+
     @Test
-    void saveStudent() {
+    public void test_saveStudent_success() {
+        String id = "1";
+        int result = service.saveStudent(id, "tester", 933);
+        assertEquals(1,result);
     }
 }
