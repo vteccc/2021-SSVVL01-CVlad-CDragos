2b0a79ec52a8c67a06c9573cd0e61795b355a570
Corocea Vlad-Laurentiu <corocea.vlad@yahoo.com>
2021-03-23 04:04:51 +0200

    added tests

--- src/test/java/ssvv/example/ServiceTest.java
+++ src/test/java/ssvv/example/ServiceTest.java
@@ -1,6 +1,9 @@
 package ssvv.example;
 
 import org.junit.jupiter.api.Test;
+import ssvv.example.domain.Nota;
+import ssvv.example.domain.Student;
+import ssvv.example.domain.Tema;
 import ssvv.example.repository.NotaXMLRepository;
 import ssvv.example.repository.StudentXMLRepository;
 import ssvv.example.repository.TemaXMLRepository;
@@ -8,6 +11,8 @@ import ssvv.example.service.Service;
 import ssvv.example.validation.NotaValidator;
 import ssvv.example.validation.StudentValidator;
 import ssvv.example.validation.TemaValidator;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,8 +25,48 @@ class ServiceTest {
 
     @Test
     public void test_saveStudent_success() {
-        String id = "1";
+        String id = "3";
         int result = service.saveStudent(id, "tester", 933);
         assertEquals(1,result);
+        service.deleteStudent(id);
+
     }
+
+    @Test
+    public void test_saveStudent_id_existent() {
+        String id = "3";
+        service.saveStudent(id, "tester", 933);
+        int result = service.saveStudent(id, "tester", 933);
+        assertEquals(0,result);
+        service.deleteStudent(id);
+    }
+
+    @Test
+    public void test_saveStudent_empty_name() {
+        String id = "3";
+        int result = service.saveStudent(id, "", 933);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void test_saveStudent_invlaid_grupa_lower() {
+        String id = "3";
+        int result = service.saveStudent(id, "", 1);
+        assertEquals(1,result);
+    }
+    @Test
+    public void test_saveStudent_invlaid_grupa_upper() {
+        String id = "3";
+        int result = service.saveStudent(id, "", 990);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void test_saveStudent_null_name() {
+        String id = "3";
+        int result = service.saveStudent(id, null, 933);
+        assertEquals(1,result);
+    }
+
+
 }
